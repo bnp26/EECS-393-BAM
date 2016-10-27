47f28330939957ed8becbb1d9b7b8eda9adf27b5
Benjamin Poreh <bnp26@case.edu>
2016-10-27 17:00:21 -0400

    added some more constructors to the line Object.

--- Jott/src/Jott/Line.java
+++ Jott/src/Jott/Line.java
@@ -2,21 +2,40 @@ package Jott;
 
 import java.util.LinkedList;
 
+import javafx.scene.layout.HBox;
+
 public class Line {
 	
 	private int lineNum;
 	private LinkedList<Word> words;
 	private boolean isEdited;
+	private HBox lineContainer;
 	
 	public Line() {
 		lineNum = 0;
 		words = new LinkedList<Word>();
 		isEdited = true;
+		lineContainer = new HBox();
 	}
 	
 	public Line(int line, LinkedList<Word> words) {
 		this.lineNum = line;
 		this.words = words;
+		this.isEdited = true;
+		lineContainer = new HBox();
+	}
+	
+	public Line(int line, HBox container) {
+		this.lineNum = line;
+		this.lineContainer = container;
+		this.isEdited = true;
+		this.words = new LinkedList<Word>();
+	}
+	
+	public Line(int line, LinkedList<Word> words, HBox container) {
+		this.lineNum = line;
+		this.words = words;
+		this.lineContainer = container;
 		this.isEdited = true;
 	}
 	
