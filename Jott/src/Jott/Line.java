422be9444ee558951abdf017f0a940559f3c0590
Benjamin Poreh <bnp26@case.edu>
2016-11-26 19:51:06 -0500

    changed font to monospace for lines, ACTUALLY works now

--- Jott/src/Jott/Line.java
+++ Jott/src/Jott/Line.java
@@ -43,7 +43,7 @@ public class Line {
 		//line.setBorder(new Border(new BorderStroke(Paint.valueOf("black"), null, new CornerRadii(1.0), BorderStroke.THIN)));
 		line.setPrefSize(Page.PAGE_WIDTH, LINE_HEIGHT);line.setMaxSize(Page.PAGE_WIDTH, 16.0);
 		line.setScaleZ(1.0);
-		line.setFont(new Font("Monospaced", 14));
+		line.setFont(Font.font("Monospaced", 14));
 		line.setTextAlignment(TextAlignment.LEFT);
 		line.setVisible(true);
 	}
