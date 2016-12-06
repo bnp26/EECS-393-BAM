9d5d76f4a1492ae26d7b2de049cdc92fe26c1704
Alp Guvenir <alpguvenir@gmail.com>
2016-12-06 17:18:19 -0500

    getPageLength method added for the sake of Notebooks.java

--- Jott/src/Jott/Notebook.java
+++ Jott/src/Jott/Notebook.java
@@ -6,13 +6,21 @@ public class Notebook {
 	
 	private String name; // this is equal to the key in the key/value pair in the notebooks pane HashMap.
 	private PagesPane pagePane;
-	
+	MongoDB mdb = new MongoDB();
+
 	public Notebook() {
 		name = null;
 	}
 	
 	public Notebook(String name) {
 		this.name = name;
+                
+                // need to create a notebook in such a way
+                // there is no way to create a DB in mongo
+                // need to create it with a collection or a instance of data
+                // we are not going to show this page when we are viewing the pages
+                
+                //mdb.createPage(name, "*!!!!*");
 		this.pagePane = new PagesPane();
 	}
 	
@@ -26,7 +34,19 @@ public class Notebook {
 	 *@return 
 	 *
 	*/
-	public void populateNotebook() {}
+	public void populateNotebook() {
+            /*
+            ArrayList<String> notebookList = mdb.getNotebooks();
+            if (arrayList.contains("Notebook Name") {
+                ArrayList<String> pageList = mdb.getPages("Notebook name");
+            }
+            
+            // If we just want to get what is inside a notebook
+            // call getPages() 
+            // and than for each page desired
+            // call getLine()
+            */
+        }
 	
 	/**
 	 * returns the page object of the passed pageID.
@@ -37,7 +57,26 @@ public class Notebook {
 	 * @see Page
 	 */
 	public Page getPage(String pageID) {
-		return null;
+            /*
+            ArrayList<String> notebookList = mdb.getNotebooks();
+            if (arrayList.contains("Notebook Name") {
+                ArrayList<String> pageList = mdb.getPages("Notebook Name");
+            
+                if (pageList.size()>1)
+                    pageList.remove("*!!!!*") //Remove the dummy page
+                    // However this should be done if there is more than one page,
+                    // else the whole DB is dropped
+            
+                pageList.get(pageID); // This will only give you the name of the page
+                Then you can use the name of the pagee for line operations
+            
+                // However to do stuff like insertLine, deleteLine etc.
+                // we call it with arguments 
+                // ADD A LINE: String notebook, String page, int linenum, String linestr
+                // DELETE A LINE: String notebook, String page, int linenum
+            }
+            */
+            return null;
 	}
 	
 	/**
@@ -48,7 +87,11 @@ public class Notebook {
 	 * 
 	 */
 	public String createPage(String newName) { return null;}
-	
+            /*
+                mdb.createPage("Notebook name", "Page name");
+            */
+
+        
 	/**
 	 * <p>Renames the currently selected Notebook to the entered parameter.</p>
 	 *
@@ -56,6 +99,9 @@ public class Notebook {
 	 *
 	 * @return void
 	 */
+        
+        // Instead of renaming a notebook we have a method for renaming a page!?
+        
 	public void renameNotebook(String newName) {
 		name = newName;
 	}
