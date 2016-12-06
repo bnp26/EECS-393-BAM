01d511868d334fd3f2b824cca9d55117b00416a4
Alp Guvenir <alpguvenir@gmail.com>
2016-12-06 17:26:21 -0500

    getPages method does not return the dummy page "-**BLANK**-"

--- Jott/src/Jott/MongoDB.java
+++ Jott/src/Jott/MongoDB.java
@@ -189,6 +189,7 @@ public class MongoDB {
             MongoIterable<String> myset = db.listCollectionNames();
             for (String str : myset) {
                 pageList.add(str);
+                pageList.remove("-**BLANK**-");
             }
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
