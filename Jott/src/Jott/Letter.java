e86dfca371cb8ecbee9f49a4f35f5010e31c75d4
Alp Guvenir <alpguvenir@gmail.com>
2016-12-06 21:38:32 -0500

    commented Letter.java & Location.java

--- Jott/src/Jott/Letter.java
+++ Jott/src/Jott/Letter.java
@@ -7,30 +7,38 @@ public class Letter extends Glyph{
 
 	private char value;
 
+        // Constructor for Letter takes a character as an argument
 	public Letter(char val) {
 		super();
 		value = val;
-        Label newLabel = setUpLabel(value);
+                // Create a Label instance with the character value
+                Label newLabel = setUpLabel(value);
 	}
 	
+        // Constructor for Letter that takes character and previous Glyph instance
 	public Letter(char val, Glyph previous) {
 		super();
 		value = val;
+                // setting the instance to the previous Glyph
 		this.setPrevious(previous);
 	}
 
+        // Letter Constrcutor with character next&previous Glpyhs
 	public Letter(char val, Glyph next, Glyph previous) {
 		super();
 		value = val;
+                // setting the next&previous Glyphs of the Letter
 		this.setNext(next);
 		this.setPrevious(previous);
 	}
 
 	private Label setUpLabel(char val) {
-        Label newLabel = new Label();
-        newLabel.setText(new String(val+""));
-        return newLabel;
-    }
+            Label newLabel = new Label();
+            // Setting the Label instance with 
+            // text value of character passed in the argument
+            newLabel.setText(new String(val+""));
+            return newLabel;
+        }
 	
 	public char getValue() {
 		return value;
