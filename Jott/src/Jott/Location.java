02ba30035c534dafc5f5ea151e8fca6aecf7c229
Alp Guvenir <alpguvenir@gmail.com>
2016-12-06 21:38:45 -0500

    commented Letter.java & Location.java

--- Jott/src/Jott/Location.java
+++ Jott/src/Jott/Location.java
@@ -2,6 +2,7 @@ package Jott;
 
 public class Location {
 
+    // Constant values for Height and Width for a character/letter
     private final double LETTER_HEIGHT = 18;
     private final double LETTER_WIDTH = 8.40625;
 
@@ -9,12 +10,15 @@ public class Location {
 //	private int wordNum;
 	private int letterNum;
 
+    // Constructor for location, initializing to 0
     public Location() {
         lineNum = 0;
   //      wordNum = 0;
         letterNum = 0;
     }
 
+    // Constructor for location with arguments
+    // line&letter
 	public Location(int line, int letter) {
 		lineNum = line;
 //		wordNum = word;
@@ -52,30 +56,42 @@ public class Location {
 		letterNum = letter;
 	}
 
+        // Method for converting the pixels given in the argument
+        // to Location instance
 	public Location pixelsToLocation(double x, double y) {
-		Location loc = new Location(0,0);
+            Location loc = new Location(0,0);
+            // Calculating the Double values for 
+            // lineNumber&letterNumber
+            Double lineNumber = new Double(y/LETTER_HEIGHT);
+            Double letterNumber = new Double(x/LETTER_WIDTH);
 
-        Double lineNumber = new Double(y/LETTER_HEIGHT);
-        Double letterNumber = new Double(x/LETTER_WIDTH);
+            // creating integer values from the doubles
+            int intLineNumber = lineNumber.intValue();
+            int intLetterNumber = letterNumber.intValue();
 
-        int intLineNumber = lineNumber.intValue();
-        int intLetterNumber = letterNumber.intValue();
+            System.out.println("("+intLineNumber + ", " + intLetterNumber+")");
 
-        System.out.println("("+intLineNumber + ", " + intLetterNumber+")");
-
-        loc.changeLocation(intLineNumber, intLetterNumber);
+            // Call changeLocation method with the new values
+            // calculated for lineNumber & letterNumber
+            loc.changeLocation(intLineNumber, intLetterNumber);
 		return loc;
 	}
 
+        // Return the X-pixel value for the location
 	public double getXPixelValue() {
-        double xLoc = 0;
-        xLoc = LETTER_WIDTH * letterNum;
-        return xLoc;
-    }
+            double xLoc = 0;
+            // Multiply the letterNum with the constant value
+            // for a single character
+            xLoc = LETTER_WIDTH * letterNum;
+            return xLoc;
+        }
 
-    public double getYPixelValue() {
-		//the 2 is a constant to offset the cursor a little.
-        double yLoc = LETTER_HEIGHT * lineNum + 2;
-        return yLoc;
-    }
+        // Return the Y-pixel value for the location
+        public double getYPixelValue() {
+            //the 2 is a constant to offset the cursor a little.
+            // Multiply the lineNum with the constant value
+            // for a single character
+            double yLoc = LETTER_HEIGHT * lineNum + 2;
+            return yLoc;
+        }
 }
