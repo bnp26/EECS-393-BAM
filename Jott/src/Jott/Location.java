2164b8b1622d82fc807fc4bc36645d8e9fb66f41
Benjamin Poreh <bnp26@case.edu>
2016-12-09 17:24:38 -0500

    changed some stuff about how the cursor moves again

--- Jott/src/Jott/Location.java
+++ Jott/src/Jott/Location.java
@@ -5,7 +5,7 @@ import javafx.stage.Screen;
 public class Location {
 
     // Constant values for Height and Width for a character/letter
-    private final double LETTER_HEIGHT = Screen.getPrimary().getVisualBounds().getHeight()/(58+(5/9));
+    private final double LETTER_HEIGHT = Screen.getPrimary().getVisualBounds().getHeight()/58.5;
     private final double LETTER_WIDTH = Screen.getPrimary().getVisualBounds().getWidth()/227.5;
 
 	private int lineNum;
